bc3c6b8ab8ae3ee41c67f829e12dd8fbf016edd9
RJ <joyce8@umbc.edu>
2019-01-31 10:25:13 -0500

    Updated RichPE hash specification - see issue #5

--- richpe.py
+++ richpe.py
@@ -1,7 +1,8 @@
 import os
 import sys
-import pefile
 import hashlib
+from pefile import PE
+from struct import pack
 
 
 """
@@ -17,7 +18,7 @@ def get_richpe(file_path=None, data=None):
 
     # Validate PE file
     try:
-        pe = pefile.PE(name=file_path, data=data, fast_load=True)
+        pe = PE(name=file_path, data=data, fast_load=True)
     except Exception:
         return None
 
@@ -29,34 +30,30 @@ def get_richpe(file_path=None, data=None):
     if len(rich_fields) % 2 != 0:
         return None
 
-    # Make a list of Rich header and PE header features
-    features = []
+    # Compute md5 digest of Rich header and PE header features
+    md5 = hashlib.md5()
     while len(rich_fields):
         compid = rich_fields.pop(0)
         count = rich_fields.pop(0)
         mask = 2 ** (count.bit_length() // 2 + 1) - 1
         count |= mask
-        features.append(str(compid))
-        features.append(str(count))
+        md5.update(pack("<L", compid))
+        md5.update(pack("<L", count))
 
-    features.append(str(pe.FILE_HEADER.Machine))
-    features.append(str(pe.FILE_HEADER.Characteristics))
-    features.append(str(pe.OPTIONAL_HEADER.Subsystem))
-    features.append(str(pe.OPTIONAL_HEADER.MajorLinkerVersion))
-    features.append(str(pe.OPTIONAL_HEADER.MinorLinkerVersion))
-    features.append(str(pe.OPTIONAL_HEADER.MajorOperatingSystemVersion))
-    features.append(str(pe.OPTIONAL_HEADER.MinorOperatingSystemVersion))
-    features.append(str(pe.OPTIONAL_HEADER.MajorImageVersion))
-    features.append(str(pe.OPTIONAL_HEADER.MinorImageVersion))
-    features.append(str(pe.OPTIONAL_HEADER.MajorSubsystemVersion))
-    features.append(str(pe.OPTIONAL_HEADER.MinorSubsystemVersion))
+    md5.update(pack("<L", pe.FILE_HEADER.Machine))
+    md5.update(pack("<L", pe.FILE_HEADER.Characteristics))
+    md5.update(pack("<L", pe.OPTIONAL_HEADER.Subsystem))
+    md5.update(pack("<B", pe.OPTIONAL_HEADER.MajorLinkerVersion))
+    md5.update(pack("<B", pe.OPTIONAL_HEADER.MinorLinkerVersion))
+    md5.update(pack("<L", pe.OPTIONAL_HEADER.MajorOperatingSystemVersion))
+    md5.update(pack("<L", pe.OPTIONAL_HEADER.MinorOperatingSystemVersion))
+    md5.update(pack("<L", pe.OPTIONAL_HEADER.MajorImageVersion))
+    md5.update(pack("<L", pe.OPTIONAL_HEADER.MinorImageVersion))
+    md5.update(pack("<L", pe.OPTIONAL_HEADER.MajorSubsystemVersion))
+    md5.update(pack("<L", pe.OPTIONAL_HEADER.MinorSubsystemVersion))
 
-    # Close PE file
+    # Close PE file and return RichPE hash digest
     pe.close()
-
-    # Generate RichPE hash from features
-    md5 = hashlib.md5()
-    md5.update(" ".join(features).encode("ascii"))
     return md5.hexdigest()
 
 
